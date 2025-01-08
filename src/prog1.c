68c6801cbf77cce1adce8021b47b2d0c257c9625
Oleh56 <olkozub56@gmail.com>
2025-01-08 23:14:38 +0200

    Coursework files added

---
+++ src/prog1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main() 
+{
+   int16_t aAAAAAAAAAAAAAAAA, bBBBBBBBBBBBBBBBB, xXXXXXXXXXXXXXXXX, yYYYYYYYYYYYYYYYY;
+   printf("Enter aAAAAAAAAAAAAAAAA:");
+   scanf("%hd", &aAAAAAAAAAAAAAAAA);
+   printf("Enter bBBBBBBBBBBBBBBBB:");
+   scanf("%hd", &bBBBBBBBBBBBBBBBB);
+   printf("%d\n", aAAAAAAAAAAAAAAAA + bBBBBBBBBBBBBBBBB);
+   printf("%d\n", aAAAAAAAAAAAAAAAA - bBBBBBBBBBBBBBBBB);
+   printf("%d\n", aAAAAAAAAAAAAAAAA * bBBBBBBBBBBBBBBBB);
+   printf("%d\n", aAAAAAAAAAAAAAAAA / bBBBBBBBBBBBBBBBB);
+   printf("%d\n", aAAAAAAAAAAAAAAAA % bBBBBBBBBBBBBBBBB);
+   xXXXXXXXXXXXXXXXX = (aAAAAAAAAAAAAAAAA - bBBBBBBBBBBBBBBBB) * 10 + (aAAAAAAAAAAAAAAAA + bBBBBBBBBBBBBBBBB) / 10;
+   yYYYYYYYYYYYYYYYY = xXXXXXXXXXXXXXXXX + (xXXXXXXXXXXXXXXXX % 10);
+   printf("%d\n", xXXXXXXXXXXXXXXXX);
+   printf("%d\n", yYYYYYYYYYYYYYYYY);
+   system("pause");
+    return 0;
+}
---
+++ src/prog2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main() 
+{
+   int16_t aAAAAAAAAAAAAAAAA, bBBBBBBBBBBBBBBBB, cCCCCCCCCCCCCCCCC;
+   printf("Enter aAAAAAAAAAAAAAAAA:");
+   scanf("%hd", &aAAAAAAAAAAAAAAAA);
+   printf("Enter bBBBBBBBBBBBBBBBB:");
+   scanf("%hd", &bBBBBBBBBBBBBBBBB);
+   printf("Enter cCCCCCCCCCCCCCCCC:");
+   scanf("%hd", &cCCCCCCCCCCCCCCCC);
+   if ((aAAAAAAAAAAAAAAAA > bBBBBBBBBBBBBBBBB))
+   {
+   if ((aAAAAAAAAAAAAAAAA > cCCCCCCCCCCCCCCCC))
+   {
+   goto Abigger;
+   }
+   else
+   {
+   printf("%d\n", cCCCCCCCCCCCCCCCC);
+   goto OutofIF;
+Abigger:
+   printf("%d\n", aAAAAAAAAAAAAAAAA);
+   goto OutofIF;
+   }
+   }
+   if ((bBBBBBBBBBBBBBBBB < cCCCCCCCCCCCCCCCC))
+   {
+   printf("%d\n", cCCCCCCCCCCCCCCCC);
+   }
+   else
+   {
+   printf("%d\n", bBBBBBBBBBBBBBBBB);
+   }
+OutofIF:
+   if (((aAAAAAAAAAAAAAAAA == bBBBBBBBBBBBBBBBB) && (aAAAAAAAAAAAAAAAA == cCCCCCCCCCCCCCCCC) && (bBBBBBBBBBBBBBBBB == cCCCCCCCCCCCCCCCC)))
+   {
+   printf("%d\n", 1);
+   }
+   else
+   {
+   printf("%d\n", 0);
+   }
+   if (((aAAAAAAAAAAAAAAAA < 0) || (bBBBBBBBBBBBBBBBB < 0) || (cCCCCCCCCCCCCCCCC < 0)))
+   {
+   printf("%d\n", -1);
+   }
+   else
+   {
+   printf("%d\n", 0);
+   }
+   if ((!(aAAAAAAAAAAAAAAAA < (bBBBBBBBBBBBBBBBB + cCCCCCCCCCCCCCCCC))))
+   {
+   printf("%d\n", (10));
+   }
+   else
+   {
+   printf("%d\n", (0));
+   }
+   system("pause");
+    return 0;
+}
